8b2dbb408c91ee5370eb684270c4e31b4c52c956
Steve Phelps <79sphelps@gmail.com>
2024-06-02 10:07:26 -0700

    adding the real estate site

---
+++ src/app/portfolio/real-estate-site/real-estate-site.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-real-estate-site',
+  templateUrl: './real-estate-site.component.html',
+  styleUrls: ['./real-estate-site.component.css']
+})
+export class RealEstateSiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
